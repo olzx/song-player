6aa2f90b6ab9aece23a2034d72a957fa2afe5bfe
Olzx <ol2az@yandex.ru>
2022-10-17 20:55:27 +0700

    added display of songs

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
     <div id="app">
-        <h1> {{ data }} </h1>
+        <SongsItems></SongsItems>
     </div>
 </template>
 
 <script>
+import SongsItems from './components/SongsItems.vue'
+
 export default {
     data: function() {
         return {
             data: 'Hello World'
         }
+    },
+    components: {
+        SongsItems
     }
 }
 </script>
 
 <style>
+@import './assets/icomoon/style.css';
+@font-face {
+    font-family: 'DinDisplayProLight';
+    src: url('./assets/PFDinDisplayPro\ Light.ttf');
+}
+body {
+    font-family: DinDisplayProLight;
+}
 
+#app {
+    width: 727px;
+    margin: 0 auto;
+}
 </style>
