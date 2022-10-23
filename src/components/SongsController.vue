37354d32f78fd2e3659e90efc6c740437a853c24
Olzx <ol2az@yandex.ru>
2022-10-23 15:03:04 +0700

    added component controller for songs

---
+++ src/components/SongsController.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="bar">
+        <div class="bar__content">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.bar {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+
+    &__content {
+        width: 100%;
+        max-width: 1000px;
+        height: 100%;
+        background-color: rgba(24,24,24,.95);
+        margin: 0 auto;
+    }
+}
+</style>
