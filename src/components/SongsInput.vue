bdc3ca69bab1b74e4c3d96b6ab7a4599cb1deb99
Olzx <ol2az@yandex.ru>
2022-11-08 18:33:26 +0700

    added new component for search

---
+++ src/components/SongsInput.vue
@@ -0,0 +1,45 @@
+<template>
+    <label class="search">
+        <input class="search__input" type="search" placeholder="Поиск треков...">
+    </label>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.search {
+    position: relative;
+
+    &:before {
+        content: '';
+        position: absolute;
+        width: 18px;
+        height: 18px;
+        background-image: url('../assets/icons/search.svg');
+        background-repeat: no-repeat;
+        top: 2px;
+        right: 20px;
+    }
+
+    &__input {
+        border: 1px solid #e6e6e6;
+        border-radius: 10px;
+        color: #111;
+        width: 100%;
+        height: 60px;
+        font-size: 18px;
+        padding: 0 40px 0 20px;
+        font-family: DinDisplayProLight;
+        transition: border-color .2s linear;
+
+        &:hover {
+            cursor: text;
+            border-color: #111;
+        }
+    }
+}
+</style>
